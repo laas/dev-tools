370c4f7ab2197654a07d6ab2e94a8a80d082b03c
Thomas Moulard <thomas.moulard@gmail.com>
2010-10-17 19:22:37 +0200

    Start CMake tempate-project.

---
+++ template-project/cmake/tests/simple-test.cc
@@ -0,0 +1,28 @@
+// Copyright (C) 2009 by &FULLNAME&.
+//
+// This file is part of the &PACKAGE_TARNAME&.
+//
+// &PACKAGE_TARNAME& is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// &PACKAGE_TARNAME& is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with &PACKAGE_TARNAME&.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "common.hh"
+
+int run_test ();
+
+int run_test ()
+{
+  //FIXME: make a real test.
+  return 0;
+}
+
+GENERATE_TEST ()
